febe78c60e1cc629050465f858c0134e9461396b
CodeSplash7 <codesplash7@mgmail.com>
2025-07-25 20:48:17 +0300

    fix(home-page): contradictory styles

--- src/page-components/home-page.tsx
+++ src/page-components/home-page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <div className="max-w-2xl mx-auto px-4 md:px-4 px-0 py-6 md:py-6 py-0 space-y-4 md:space-y-4 space-y-0">
+      <div className="max-w-2xl mx-auto px-4 md:px-4 py-6 md:py-6 space-y-4 md:space-y-4">
         <PostPrompt />
         <Posts />
       </div>
